e00efcbf639688a28fa89b47a4c7b3ca73beeb33
Jesus Hernandez <jh376490@gmail.com>
2026-03-15 12:33:40 -0600

    Remove unused CSS/JS and add SEO & Open Graph metadata

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         "Download-Button": resolve("Download-Button/index.html"),
-        "Send-Button": resolve("Send-Button/index.html"),
+        "Navbar-Icons": resolve("Navbar-Icons/index.html"),
         "Neon-Button": resolve("Neon-Button/index.html"),
+        "Send-Button": resolve("Send-Button/index.html"),
       },
     },
   },
